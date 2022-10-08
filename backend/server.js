6db335e8ebb577508e7b7b30b56689190737305f
Tony Damari <74777498+TonyDamari@users.noreply.github.com>
2022-10-08 17:05:56 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,10 @@ connectDB();
 //Midddleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("frontend/build"));
+
 //
 app.get("/", (req, res) => {
-    res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
-        );
+   res.send("Camming Desk");
 });
 
 //Routes
